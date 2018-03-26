78d428cee777c8b098a0bf8e30ba2b69555522b5
batman <salat@batmans-Mac-mini.local>
2018-03-26 10:20:23 +0300

    add another version of convertor

--- runner.js
+++ runner.js
@@ -1,10 +1,12 @@
-const url = 'https://1af55ced.ngrok.io'
+const url = 'https://53e88845.ngrok.io'
 
+const { record } = require('./videoCreation')
 const puppeteer = require('puppeteer')
 
 let start = async () => {
   const browser = await puppeteer.launch({
     headless: false,
+    slowMo: 500,
     // args: ['--start-fullscreen']
     args: ['--window-size=1280,720']
   })
@@ -23,33 +25,53 @@ let start = async () => {
   })
   // Initial frame
   // await page.screenshot({path: './frames/page0.png'})
-  var frame = 0
-  let worker = function (resolve, reject) {
-    frame++
-    // Render an image with the frame name
-    console.log(frame)
-    try {
-      page.screenshot({path: './frames/page0' + frame + '.png'})
-    } catch (e) {
-      console.error(e)
-    }
-
-    if (frame > 500) {
-      console.log('Done')
-      resolve()
-    } else {
-      worker(resolve, reject)
-    }
+  // page.startScreencast
+  try {
+    await record({
+      browser: browser, // Optional: a puppeteer Browser instance,
+      page: page, // Optional: a puppeteer Page instance,
+      output: 'output.webm',
+      fps: 24,
+      frames: 72, // 5 seconds at 60 fps
+      prepare: async function (browser, page) {
+        console.log('_______________PREPARE_______________')
+      },
+      render: function (browser, page, frame) {
+        console.log(frame)
+      }
+    })
+  } catch (e) {
+    console.error(e)
   }
-  let makeScreenshot = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      worker(resolve, reject)
-    }, 15)
-  })
-  await Promise.all([makeScreenshot]).then(() => {
-    console.log(3423)
-  })
-  // await browser.close()
+
+  // var frame = 0
+  // let worker = function (resolve, reject) {
+  //   frame++
+  //   // Render an image with the frame name
+  //   console.log(frame)
+  //   try {
+  //     page.screenshot({path: './frames/page0' + frame + '.png'})
+  //   } catch (e) {
+  //     console.error(e)
+  //   }
+  //
+  //   if (frame > 500) {
+  //     console.log('Done')
+  //     resolve()
+  //   } else {
+  //     worker(resolve, reject)
+  //   }
+  // }
+  // let makeScreenshot = new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     worker(resolve, reject)
+  //   }, 15)
+  // })
+  // await Promise.all([makeScreenshot]).then(() => {
+  //   console.log(3423)
+  // })
+
+  await browser.close()
 }
 
 start()
